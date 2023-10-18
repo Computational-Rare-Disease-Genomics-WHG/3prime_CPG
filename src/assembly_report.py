d2d89316a4f8a84396e4f06e13803619693bf279
Elston D'Souza <elstonndsouza@gmail.com>
2023-10-18 17:41:02 +0100

    Fix linting errors

--- src/assembly_report.py
+++ src/assembly_report.py
@@ -4,13 +4,13 @@ assembly_report.py
 This script takes the assembly report from NCBI and creates a VCF header file
 and a tab delimited file with the contigs and their lengths.
 
-python3 assembly_report.py 
+python3 assembly_report.py
     --input ../../../data/pipeline/GCA_000001405.15_GRCh38_assembly_report.txt
-    --output assembly_report.txt 
+    --output assembly_report.txt
 """
 
 import argparse
-import pandas
+import pandas  # pylint: disable=import-error
 
 
 def main(args):
@@ -19,32 +19,33 @@ def main(args):
     """
     b = pandas.read_csv(
         args.input,
-        sep="\t",
+        sep='\t',
         skiprows=61)
     b = b[['# Sequence-Name', 'RefSeq-Accn',
            'UCSC-style-name', 'Assembly-Unit',
            'Sequence-Length']]
-    b = b[b['Assembly-Unit'] == "Primary Assembly"]
-    b = b[b['# Sequence-Name'].isin([str(i) for i in list(range(1, 23))+["X", "Y"]])]
-    b.to_csv(args.output, index=False, sep="\t")
+    b = b[b['Assembly-Unit'] == 'Primary Assembly']
+    b = b[b['# Sequence-Name'].isin([str(i) for i in list(range(1, 23))+['X', 'Y']])]
+    b.to_csv(args.output, index=False, sep='\t')
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
-        description=""  # noqa: E501 # pylint: disable=C0301
+        description=''  # noqa: E501 # pylint: disable=C0301
     )
     parser.add_argument(
-        "--input",
+        '--input',
         type=str,
-        help="Input File",
+        help='Input File',
     )
     parser.add_argument(
-        "--output",
+        '--output',
         type=str,
-        help="Output file",
+        help='Output file',
     )
     parser.add_argument(
-        "--header_output",
+        '--header_output',
         type=str,
-        help="Header for the VCF file",
+        help='Header for the VCF file',
     )
     main(args=parser.parse_args())
